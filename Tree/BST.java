b9ead8c9ff54df696ec72454f90cf386fc58d077
Mri07 <mrinal7331@gmail.com>
2020-07-27 22:20:04 +0530

    ITERATIVE and RECURSIVE METHODS

--- Tree/BST.java
+++ Tree/BST.java
@@ -1,44 +1,87 @@
 package data_structures.Tree;
 
-public class BST 
+public class BST
 {
 	private Node root=null,result=null;
-	public static void main(String[] args) 
-	{		
+	public static void main(String[] args)
+	{
 		BST obj = new BST();
-		obj.input();
-	}	
-	private void input() 
+		obj.recursiveBST();
+
+		//obj.iterativeBST();
+	}
+
+	private void iterativeBST()
 	{
 		int arr[] = {10, 5, 1, 7, 40, 50};
-		
+		Node ptr = null;
+		Node x = null;
+
 		root = new Node(arr[0]);
-		
+
 		for(int i=1 ; i<arr.length ; i++)
 		{
-			 result = createBST(root,arr[i]);						
+			if( arr[i] < root.data )
+			{
+				ptr = root;
+				while( ptr != null )
+				{
+					x = ptr;
+					ptr = ptr.left;
+				}
+				x.left = new Node(arr[i]);
+			}
+			else
+			{
+				ptr = root;
+				while( ptr != null )
+				{
+					x = ptr;
+					ptr = ptr.right;
+				}
+				x.right = new Node(arr[i]);
+			}
+		}
+     show(root);
+	}
+	private void recursiveBST()
+	{
+		int arr[] = {10, 5, 1, 7, 40, 50};
+
+		//root = new Node(arr[0]);  // First way to make first element as ROOT
+
+		for(int i=0 ; i<arr.length ; i++)
+		{
+			 result = createBST(root,arr[i]);
 		}
 		show(result);
 	}
-	private void show(Node result) 
+
+	private Node createBST(Node ptr,int arr)
 	{
-		if(result == null)return ;
-		
-		System.out.println(result.data);
-		show(result.left);		
-		show(result.right);		
-	}
-	private Node createBST(Node ptr,int arr) 
-	{
+		if( root == null ) // Second way to make first element as ROOT
+		{
+			root = new Node(arr);
+			return ptr;
+		}
 		if(ptr == null)
 		{
 			ptr = new Node(arr);
 			return ptr;
 		}
-		if( arr < ptr.data ) ptr.left = createBST(ptr.left, arr);		
+		if( arr < ptr.data ) ptr.left = createBST(ptr.left, arr);
 		else ptr.right = createBST(ptr.right, arr);
-		
+
 		return ptr;
 	}
-		
+
+	private void show(Node result)
+	{
+		if(result == null)return ;
+
+		System.out.print(result.data+" ");
+		show(result.left);
+		show(result.right);
+
+	}
 }
